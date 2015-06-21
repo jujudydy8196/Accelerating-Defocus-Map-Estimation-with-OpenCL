c259f75482a960614670089b523b0dce9174c96b
jujudydy8196 <jujudydy8196@gmail.com>
2015-06-20 22:14:22 -0400

    add

--- defocusMap.sh
+++ defocusMap.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-if [ "$1" == "-h" ]; then
-	echo "Usage: `basename $0` [-h] <Input_pgm_dir>"
+if [ "$1" = "-h" ]; then
+	echo "Usage: `basename $0` [-h] <Input_pgm_dir_name>
+    <Input_pgm_name>: input image directory place in ./ImageData/ 
+    example:
+           --- src/
+           |__ ImageData/
+              |__ boy
+                 |__ input.ppm
+    ->  ./defocusMap.sh boy "
 	exit 0
 fi
 
